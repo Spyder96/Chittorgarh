836fb9a622be309322bdb8199ba80137d5908c4e
Spyder96 <Amirul.1216@gmail.com>
2023-08-11 22:47:54 +0530

    replacing selenium with pandas for data pull

--- buybackapp.py
+++ buybackapp.py
@@ -6,75 +6,56 @@ from datetime import datetime
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import requests
 
 class BuybackAnalyzerApp:
     def __init__(self):
-        # Set up the WebDriver
-        self.driver = webdriver.Chrome()
-        self.download_dir = self._setup_download_dir()
-        self.df = self._download_and_process_data()
+        url = 'https://www.chittorgarh.com/report/latest-buyback-issues-in-india/80/tender-offer-buyback/'
+        Headers=({'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/111.0' , 'Accept-language':'en-US , en;q=0.5'})
+        self.response = requests.get(url, headers=Headers)
+        self.df = self._get_data()
+        self.df = self._process_data()
         self._display_gui()
 
-    def _setup_download_dir(self):
-        user_home = os.path.expanduser("~")
-        download_dir = os.path.join(user_home, "Downloads")
-        return download_dir
+    
+    def _get_data(self):
+        if self.response.status_code == 200:
+            tables = pd.read_html(self.response.text)
+            desired_table = tables[2]
+            return desired_table
+        else:
+            print(f"Failed to retrieve data. Status code: {self.response.status_code}")
 
-    def _download_and_process_data(self):
-        # Navigate to the web page
-        self.driver.get('https://www.chittorgarh.com/report/latest-buyback-issues-in-india/80/tender-offer-buyback/')
+    def _process_data(self):
+        df = self.df
+        date_columns=['Record Date', 'Issue Open', 'Issue Close']
+        # Format string to match the input date format
+        date_input_format = "%b %d, %Y"
+        for col in date_columns:
+            df[col]= pd.to_datetime(df[col], format = date_input_format)
+        df = df[['Company Name', 'Record Date', 'Issue Open', 'Issue Close',
+                'BuyBack price (Per Share)', 'Current Market Price',
+                'Issue Size - Shares (Cr)', 'Issue Size - Amount (Cr)']]
+        df = df.dropna(subset=['Record Date'])
 
-        # Find the button element
-        button = self.driver.find_element(By.ID, 'export_btn')
+        today = pd.to_datetime(datetime.today().date())
+        df['Record Date Diff'] = (today - df['Record Date']).dt.days
+        df['Apply Date Diff'] = (today - df['Issue Open']).dt.days
+        df['Closed Date Diff'] = (today - df['Issue Close']).dt.days
+        cdf = df[(df['Closed Date Diff'] <= 0 )]
+        nadf = df[df['Closed Date Diff'].isna()]
+        df = pd.concat([cdf, nadf], ignore_index = True)
+        for col in date_columns:
+            df[col]= pd.to_datetime(df[col]).dt.date
 
-        # Execute JavaScript to trigger the download
-        self.driver.execute_script("arguments[0].click();", button)
+        df['Profit'] = df['BuyBack price (Per Share)'] - df['Current Market Price']
+        minimum_price_difference = 100
+        df = df[(df['Profit'] >= minimum_price_difference)]
 
-        # Wait for a few seconds to allow the download to start
-        time.sleep(5)
+        df = df[['Company Name', 'Record Date', 'Issue Open', 'Issue Close', 'Profit',
+                'Issue Size - Shares (Cr)', 'Issue Size - Amount (Cr)', 'BuyBack price (Per Share)', 'Current Market Price']]
 
-        # Construct the file path
-        sorted_file_list = self._get_sorted_file_list()
-        if sorted_file_list[-1].endswith(".csv"):
-            file_path = os.path.join(self.download_dir, sorted_file_list[-1])
-            df = pd.read_csv(file_path)
-
-            date_columns=['Record Date', 'Issue Open', 'Issue Close']
-            # Format string to match the input date format
-            date_input_format = "%b %d, %Y"
-            for col in date_columns:
-                df[col]= pd.to_datetime(df[col], format = date_input_format)
-            df = df[['Company Name', 'Record Date', 'Issue Open', 'Issue Close',
-                    'BuyBack price (Per Share)', 'Current Market Price',
-                    'Issue Size - Shares (Cr)', 'Issue Size - Amount (Cr)']]
-            df = df.dropna(subset=['Record Date'])
-
-            today = pd.to_datetime(datetime.today().date())
-            df['Record Date Diff'] = (today - df['Record Date']).dt.days
-            df['Apply Date Diff'] = (today - df['Issue Open']).dt.days
-            df['Closed Date Diff'] = (today - df['Issue Close']).dt.days
-            cdf = df[(df['Closed Date Diff'] <= 0 )]
-            nadf = df[df['Closed Date Diff'].isna()]
-            df = pd.concat([cdf, nadf], ignore_index = True)
-            for col in date_columns:
-                df[col]= pd.to_datetime(df[col]).dt.date
-
-            df['Profit'] = df['BuyBack price (Per Share)'] - df['Current Market Price']
-            minimum_price_difference = 100
-            df = df[(df['Profit'] >= minimum_price_difference)]
-
-            df = df[['Company Name', 'Record Date', 'Issue Open', 'Issue Close', 'Profit',
-                   'Issue Size - Shares (Cr)', 'Issue Size - Amount (Cr)', 'BuyBack price (Per Share)', 'Current Market Price']]
-
-            try:
-                # Delete the file
-                os.remove(file_path)
-                print(f"File '{file_path}' has been deleted.")
-            except FileNotFoundError:
-                print(f"File '{file_path}' not found.")
-            except Exception as e:
-                print(f"An error occurred: {e}")
-            return df
+        return df
 
     def _get_sorted_file_list(self):
         file_list = os.listdir(self.download_dir)
